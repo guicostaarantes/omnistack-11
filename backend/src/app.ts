d536f96c732b26415c1dbf2f043586b5ff11b50e
guicostaarantes <arantes.g@outlook.com>
2020-03-25 14:14:58 -0300

    added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import env from "../env";
 import express from "express";
+import cors from "cors";
 import NgoRoutes from "./routes/NgoRoutes";
 import IncidentRoutes from "./routes/IncidentRoutes";
 
@@ -7,6 +8,7 @@ env();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(NgoRoutes);
